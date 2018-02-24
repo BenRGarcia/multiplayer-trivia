df9c6b6dd720135220b900d16b251cf7e24e9a55
Ben Garcia <ben@seebenprogram.com>
2018-02-24 18:40:44 -0500

    Initial commit

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- Page layout for home page -->
+  <div>
+    <a 
+      :href="url"
+      target="_blank" 
+    >
+      {{ linkText }}
+    </a>
+    <h1>
+      {{ headingText }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: "https://github.com/BenRGarcia/multiplayer-trivia",
+      linkText: "View Source Code",
+      headingText: "TRIVIA"
+    }
+  }
+}
+</script>
+
+<style scoped>
+a:hover {
+  text-decoration: none;
+}
+</style>
