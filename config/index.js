21e90687f43ab10c9be28472013c601dec6b4064
Ben Garcia <ben@seebenprogram.com>
2018-02-24 15:46:01 -0500

    set build's public path, SourceMap false

--- config/index.js
+++ config/index.js
@@ -43,13 +43,13 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/multiplayer-trivia/',
 
     /**
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
