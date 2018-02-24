1f6d0db14441ec5f4993597fd74be33f5b898a58
Ben Garcia <ben@seebenprogram.com>
2018-02-24 18:01:01 -0500

    Add routes for home page and gameplay

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Home from '@/components/Home/Home'
+import Play from '@/components/Play/Play'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }/*,
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }*//*,
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }*//*,
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }*/
+    { // Redirect unknown paths to home
+      path: '*',
+      redirect: '/home'
+    },
+    { // Home page, contains sign-in/sign-up, leaderboard
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    { // Game page, for authenticated users
+      path: '/play',
+      name: 'Play',
+      component: Play
+    }
   ]
 })
 
 // https://medium.com/codingthesmartway-com-blog/vue-js-routing-with-vue-router-4c428fabb078
 // https://router.vuejs.org/en/
+
+/*
+Views list:
+
+1) Home   - https://benrgarcia.github.io/multiplayer-trivia/home
+
+3) Game   - https://benrgarcia.github.io/multiplayer-trivia/play
+*/
