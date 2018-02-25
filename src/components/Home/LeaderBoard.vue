87ed84284ddba1c0a8c3f7753c6ca642e42ef08d
Ben Garcia <ben@seebenprogram.com>
2018-02-24 23:07:46 -0500

    Build initial state of leaderboard component

--- src/components/Home/LeaderBoard.vue
+++ src/components/Home/LeaderBoard.vue
@@ -1,41 +1,44 @@
 <template>
-  <!-- Leaderboard Component -->
-  <div class="container">
-  <h3>LEADERBOARD</h3>
-  <table class="table table-sm">
-    <thead>
-      <tr>
-        <th scope="col">Place</th>
-        <th scope="col">Name</th>
-        <th scope="col">Points</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>34</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>17</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Larry</td>
-        <td>15</td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="row">
+    <div :class="leaderboardClasses">
+
+      <h3>LEADERBOARD</h3>
+      <table class="table table-sm">
+        <thead>
+          <tr>
+            <th scope="col">Place</th>
+            <th scope="col">Name</th>
+            <th scope="col">Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- <leader v-for="(leader, index) in leaderboard" :key="index" :place="leader.place", :playerName="leader.name", :points="leader.points"/> -->
+          <leader/>
+        </tbody>
+      </table>
+      
+    </div>
   </div>
 </template>
 
 <script>
+import leader from './leader'
+
 export default {
+  props: [
+    // leaderboard
+  ],
+  components: {
+    leader
+  },
   data() {
     return {
-      /* ... */
+      leaderboardClasses: [
+        "col-12",                   // xs screens
+        "col-sm-10", "offset-sm-1", // sm screens
+        "col-md-8", "offset-md-2",  // md screens
+        "col-lg-6", "offset-lg-3"   // xl screens
+      ]
     }
   }
 }
