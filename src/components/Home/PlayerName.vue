ca34f884f1fe24245ec8ceb5387d96c72a3a98b9
Ben Garcia <ben@seebenprogram.com>
2018-02-24 22:21:06 -0500

    Add comments for visual organization

--- src/components/Home/PlayerName.vue
+++ src/components/Home/PlayerName.vue
@@ -18,8 +18,10 @@ export default {
         "col-10", "offset-1",      // xs screens
         "col-sm-8", "offset-sm-2", // sm screens
         "col-md-6", "offset-md-3", // md screens
-        "col-lg-4", "offset-lg-4",  // lg screens
-        "border", "rounded", "pt-1", "pb-1"
+        "col-lg-4", "offset-lg-4", // lg screens
+        // Border/padding
+        "border", "rounded", 
+        "pt-1", "pb-1"
       ],
       inputLabel: "Choose Your Trivia Name",
       inputPlaceholder: "ex. QuizlyBear88",
