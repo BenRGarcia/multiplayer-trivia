e96faa85b54ce7ee28f81221bf887379b4a33fde
Ben Garcia <ben@seebenprogram.com>
2018-02-24 22:59:07 -0500

    Build initial state of leader component

---
+++ src/components/Home/leader.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <!-- <tr>
+    <th scope="row">
+      {{ place }}
+    </th>
+    <td>
+      {{ playerName }}
+    </td>
+    <td>
+      {{ points }}
+    </td>
+  </tr> -->
+
+  <!-- Hard coded dummy content -->
+  <tr>
+    <th scope="row">1</th>
+    <td>Ben</td>
+    <td>47</td>
+  </tr>
+
+</template>
+
+<script>
+export default {
+  props: [
+    // props for each singular leader on leaderboard
+    "place", "playerName", "points"
+  ],
+  data() {
+    return {
+      // ...
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
