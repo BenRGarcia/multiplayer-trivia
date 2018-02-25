59b4c57b15adb3371e913d13459fded5b0d7ef1e
Ben Garcia <ben@seebenprogram.com>
2018-02-24 21:15:32 -0500

    Initial commit

---
+++ src/components/Home/PlayerName.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row mb-3">
+  <form class="form-signin col-4 offset-4">
+    <div class="text-center mb-4">
+      <h3 class="h4 mb-3 font-weight-normal">Choose your player name to play!</h3>
+    </div>
+
+    <div class="form-label-group mb-2">
+      <input type="text" id="inputName" class="form-control" placeholder="Player Name" required="" autofocus="">
+    </div>
+
+    <button class="btn btn-primary">SUBMIT</button>
+  
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      /* ... */
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
