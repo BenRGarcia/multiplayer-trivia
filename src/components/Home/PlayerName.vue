217d9c41d61dd7cc574ea108aeb40ed6796651f1
Ben Garcia <ben@seebenprogram.com>
2018-02-24 22:32:37 -0500

    Add Vue binding for playerName input, empty method for firebase

--- src/components/Home/PlayerName.vue
+++ src/components/Home/PlayerName.vue
@@ -3,9 +3,9 @@
     <form :class="formClasses">
       <div class="form-group">
         <label>{{ inputLabel }}</label>
-        <input type="text" class="form-control" :placeholder="inputPlaceholder">
+        <input v-model.trim="playerName" class="form-control" :placeholder="inputPlaceholder">
       </div>
-      <button class="btn btn-primary">{{ buttonText }}</button>
+      <button @click="createPlayer" class="btn btn-primary">{{ buttonText }}</button>
     </form>
   </div>
 </template>
@@ -14,18 +14,28 @@
 export default {
   data() {
     return {
+      // CSS Styling
       formClasses: [
         "col-10", "offset-1",      // xs screens
         "col-sm-8", "offset-sm-2", // sm screens
         "col-md-6", "offset-md-3", // md screens
         "col-lg-4", "offset-lg-4", // lg screens
-        // Border/padding
+        // Border + padding
         "border", "rounded", 
         "pt-1", "pb-1"
       ],
+      // Element text
       inputLabel: "Choose Your Trivia Name",
       inputPlaceholder: "ex. QuizlyBear88",
-      buttonText: "Play Trivia!"
+      buttonText: "Play Trivia!",
+      // Input value/user input
+      playerName: ""
+    }
+  },
+  methods: {
+    createPlayer() {
+      // add player to database ...
+      // ... then navigate to 'play' page
     }
   }
 }
