ef88d8676f05f91fbb046b73059f1b4a3aaef149
Ben Garcia <ben@seebenprogram.com>
2018-02-27 14:18:38 -0500

    Remove dummy props

--- src/App.vue
+++ src/App.vue
@@ -23,32 +23,13 @@ import PageTitle from './components/PageTitle'
 import trivia from './assets/javascript/trivia.js'
 
 export default {
-  props: [
-    // from firebase
-    /* 
-    "leaders", "trivia", "playerStats", "timer"
-    */
-  ],
   name: 'App',
   data() {
     return {
-      // Simulated props from firebase:
-      leaders: [
-        {rank: 1,  name: "QuizzlyBear88",    points: 38},
-        {rank: 2,  name: "JawhnyBoi66",      points: 37},
-        {rank: 3,  name: "OwlyMcOwlerson",   points: 35},
-        {rank: 4,  name: "OllieOxenfree",    points: 34},
-        {rank: 5,  name: "TerribleTerry",    points: 31},
-        {rank: 6,  name: "LadyHaha72",       points: 28},
-        {rank: 7,  name: "TriviaAddict22",   points: 22},
-        {rank: 8,  name: "IssYaBoi55",       points: 20},
-        {rank: 9,  name: "CaseOfTheMundees", points: 18},
-        {rank: 10, name: "BillyTheKidGoat",  points: 16}
-      ]
+      components: {
+        PageTitle
+      }
     }
-  },
-  components: {
-    PageTitle
   }
 }
 </script>
