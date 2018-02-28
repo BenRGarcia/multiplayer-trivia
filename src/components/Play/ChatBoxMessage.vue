356e246f2a863c84f6f2fe7b54021893d68bb56d
Ben Garcia <ben@seebenprogram.com>
2018-02-28 11:31:47 -0500

    Add remarks, make human readable

--- src/components/Play/ChatBoxMessage.vue
+++ src/components/Play/ChatBoxMessage.vue
@@ -1,28 +1,21 @@
 <template>
-
   <div>
-
-  <div>
+    <!-- Actual component -->
     <p>
       <span class="name">QuizzlyBear{{ name }}</span>: You're going down!{{ message }} 
     </p>
+
+    <!-- Will delete, hard coded for initial state -->
+    <p>QuizlyBear88: You're going down!</p>
+    <p>Triviaddict76: No way! You're going down!</p>
+    <p>QuizlyBear88: Not a chance! I'm a trivia champ!</p>
+    <p>Triviaddict76: Well I'm beating you already, so there! Well I'm beating you already, so there!</p>
+    <p>QuizlyBear88: How about them apples?!</p>
+    <p>Triviaddict76: No way! You're going down!</p>
+    <p>QuizlyBear88: Not a chance! I'm a trivia champ!</p>
+    <p>Triviaddict76: Well I'm beating you already, so there! Well I'm beating you already, so there!</p>
+    <p>QuizlyBear88: How about them apples?!</p>
   </div>
-
-
-
-
-  <!-- Will delete, hard coded for initial coding -->
-  <p>QuizlyBear88: You're going down!</p>
-  <p>Triviaddict76: No way! You're going down!</p>
-  <p>QuizlyBear88: Not a chance! I'm a trivia champ!</p>
-  <p>Triviaddict76: Well I'm beating you already, so there! Well I'm beating you already, so there!</p>
-  <p>QuizlyBear88: How about them apples?!</p>
-  <p>Triviaddict76: No way! You're going down!</p>
-  <p>QuizlyBear88: Not a chance! I'm a trivia champ!</p>
-  <p>Triviaddict76: Well I'm beating you already, so there! Well I'm beating you already, so there!</p>
-  <p>QuizlyBear88: How about them apples?!</p>
-
-</div>
 </template>
 
 <script>
