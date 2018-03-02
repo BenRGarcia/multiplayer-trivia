1d37d005ba195cfe9d4212bfaff31de35ba706d4
Ben Garcia <ben@seebenprogram.com>
2018-03-02 10:39:11 -0500

    Add method 'postQuestion'

--- src/components/Host/PostQuestion.vue
+++ src/components/Host/PostQuestion.vue
@@ -4,14 +4,18 @@
       <h5 class="card-title">Post Question</h5>
       <p class="card-text mb-1">Click the button below to post the next trivia question.</p> 
       <p class="card-text">(This will also start the timer automatically)</p>
-      <button @click="" class="btn btn-primary">Post Next Question</button>
+      <button @click="postQuestion" class="btn btn-primary">Post Next Question</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    postQuestion() {
+      this.$emit("postQuestion");
+    }
+  }
 }
 </script>
 
