1ffde9913bc9a1aecf63cfd0f7bc3e486acc68ab
Ben Garcia <ben@seebenprogram.com>
2018-03-02 16:32:20 -0500

    Refactor sendMessage method, delete excess comments

--- src/components/Play/ChatBoxMessageInput.vue
+++ src/components/Play/ChatBoxMessageInput.vue
@@ -36,21 +36,17 @@ export default {
     sendMessage() {
       // Declare local variable
       let message = this.newMessage;
-
       // Reset message input to empty
       this.newMessage = "";
-
-      // Don't register messages unless player name is set
-      if (localStorage.getItem("playerName")) {
-        
+      // Don't register messages unless playerKey is set
+      if (localStorage.getItem("playerKey")) {
         return message.length <= this.maxChars
           ? this.$emit("sendMessage", message)
           : alert("Max character limit is 100 characters");
-
-      } else {
-        alert("You must create your player name before sending messages");
-        return null;
       }
+      // player name not yet set
+      alert("You must create your player name before sending messages");
+      return message;
     }
   }
 }
