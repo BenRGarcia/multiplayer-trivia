080e33e15322cca49a8bf4a94faf1a12abba32f3
Ben Garcia <ben@seebenprogram.com>
2018-03-02 12:52:34 -0500

    Adjust bottom padding

--- src/components/Host/TriviaData.vue
+++ src/components/Host/TriviaData.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <div class="card-header">
+    <div class="card-header pt-1 pb-1">
       <h2 class="mb-0">Trivia Data</h2>
     </div>
-    <div class="card-body pb-1">
-      <p class="card-text">
+    <div class="card-body pb-1 pt-2">
+      <p class="card-text mb-1">
         Seconds Remaining: 
         <span v-for="time in timer">{{ time.remaining }}</span>
       </p>
-      <p>
+      <p class="mb-1">
         <button class="btn btn-outline-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-          Click to see current players and scores
+          Toggle | Current players and scores
         </button>
       </p>
       <div class="collapse" id="collapseExample">
